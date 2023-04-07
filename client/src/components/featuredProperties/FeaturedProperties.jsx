ebc6f5b42c4279cd4db461fb803938724a3fbd74
alanaynbund <aynbunda@gmail.com>
2023-04-07 13:53:17 -0400

    featured properties

---
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,50 @@
+import "./featuredProperties.css"
+
+const FeaturedProperties = () => {
+    return(
+        <div className="fp">
+            <div className="fpItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/286659200.webp?k=9206fc9239b3e4538c22d04b85213d6d5e6257015022de8a37effd956fcde4b6&o=&s=1" className="fpImg" alt="" />
+                <span className="fpName">La Roulotte de Ciney</span>
+                <span className="fpCity">Belgium, Ciney</span>
+                <span className="fpPrice">$200</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="fpItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/286659200.webp?k=9206fc9239b3e4538c22d04b85213d6d5e6257015022de8a37effd956fcde4b6&o=&s=1" className="fpImg" alt="" />
+                <span className="fpName">La Roulotte de Ciney</span>
+                <span className="fpCity">Belgium, Ciney</span>
+                <span className="fpPrice">$200</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="fpItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/286659200.webp?k=9206fc9239b3e4538c22d04b85213d6d5e6257015022de8a37effd956fcde4b6&o=&s=1" className="fpImg" alt="" />
+                <span className="fpName">La Roulotte de Ciney</span>
+                <span className="fpCity">Belgium, Ciney</span>
+                <span className="fpPrice">$200</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="fpItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/286659200.webp?k=9206fc9239b3e4538c22d04b85213d6d5e6257015022de8a37effd956fcde4b6&o=&s=1" className="fpImg" alt="" />
+                <span className="fpName">La Roulotte de Ciney</span>
+                <span className="fpCity">Belgium, Ciney</span>
+                <span className="fpPrice">$200</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FeaturedProperties
