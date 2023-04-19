fec633302526c2b83254b1079bbf9bbd3c916840
alanaynbund <aynbunda@gmail.com>
2023-04-19 17:02:59 -0400

    more queries

--- server/controllers/hotelController.js
+++ server/controllers/hotelController.js
@@ -40,15 +40,18 @@ export const getHotel = async (req, res, next)=>{
         next(err)
     }
 }
-export const getAllHotels = async (req, res, next)=>{
-    try{
-        const hotels = await Hotel.find()
-        res.status(200).json(hotels)
-
-    }catch(err){
-        next(err)
+export const getAllHotels = async (req, res, next) => {
+    const { min, max, ...others } = req.query;
+    try {
+      const hotels = await Hotel.find({
+        ...others,
+        cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+      }).limit(req.query.limit)
+      res.status(200).json(hotels)
+    } catch (err) {
+      next(err)
     }
-}
+  };
 
 export const countByCity = async (req, res, next)=>{
     const cities = req.query.cities.split(",")
@@ -73,11 +76,11 @@ export const countByType = async (req, res, next)=>{
 
 
         res.status(200).json([
-            {type:"hotel", count:hotelCount},
-            {type:"apartment", count:apartmentCount},
-            {type:"resort", count:resortCount},
-            {type:"villa", count:villaCount},
-            {type:"cabin", count:cabinCount}
+            {type:"hotels", count:hotelCount},
+            {type:"apartments", count:apartmentCount},
+            {type:"resorts", count:resortCount},
+            {type:"villas", count:villaCount},
+            {type:"cabins", count:cabinCount}
         ])
 
     }catch(err){
