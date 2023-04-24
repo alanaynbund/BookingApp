8c6a29048554acaa1e5557e78a0373771479e8b2
alanaynbund <aynbunda@gmail.com>
2023-04-24 18:27:48 -0400

    room numbers

--- server/controllers/hotelController.js
+++ server/controllers/hotelController.js
@@ -91,10 +91,11 @@ export const countByType = async (req, res, next)=>{
 
 export const getHotelRooms = async (req, res, next) =>{
     try{
-        const hotel = Hotel.findById(req.params.id)
-        const list = Promise.all(hotel.rooms.map(room=> {
+        const hotel = await Hotel.findById(req.params.id)
+        const list = await Promise.all(hotel.rooms.map(room=> {
             return Room.findById(room)
         }))
+        res.status(200).json(list)
     }catch(err){
         next(err)
     }
--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -4,14 +4,14 @@ import {createError} from "../utils/error.js"
 
 export const createRoom = async (req,res, next) => {
 
-    const hotelId = req.params.hotelId
+    const hotelId = req.params.hotelid
     const newRoom = new Room(req.body)
 
     try{
         const savedRoom = await newRoom.save()
         try{
             await Hotel.findByIdAndUpdate(hotelId, {
-                $push: {rooms: savedRoom._id}
+                $push: {rooms: savedRoom._id},
             })
         }catch(err){
             next(err)
